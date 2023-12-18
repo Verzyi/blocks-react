c69ba4428d51657c11f5d9662f60aa602b0a1e29
Chris Becker <cjbgamer555@gmail.com>
2023-12-18 16:37:10 -0600

    add three components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Game from "./components/Game";
+import Canvas from "./components/Canvas";
+import Score from "./components/Score";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App</h1>
+      <Game />
+      <Canvas />
+      <Score />
     </div>
   );
-}
+};
 
 export default App;
